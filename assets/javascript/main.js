0d3129353833372db3bcadbaec4cc858e9c00eb5
Christopher-Adzima_lmig <Christopher.Adzima@LibertyMutual.com>
2023-04-18 09:03:37 -0400

    Updated display of results and adjusted sizing for mobile given simplicity of site.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -6,29 +6,31 @@ const date = document.querySelector("#date");
 
 // When the Calcuate button is clicked button is clicked
 calcDate.addEventListener("click", () => {
-  // Get difference between entered dated and todays date
+  // Get difference between entered dated and todays date in milliseconds
   let diff = calculateDays(date.value);
 
   // Figure out if its a future date or not
-  let isFutureDate =  (diff < 0) ? true : false;
+  let isFutureDate = diff < 0 ? true : false;
 
-  // Convert the milliseconds to days
+  // Convert the milliseconds to days - use this if you want to just display days
   let days = convertMiliseconds(diff, "d");
+
+  // this returns array with days, weeks, years - doesnt use months because of variability
+  let dates = altConvertMiliseconds(diff);
 
   // Convert the input date to mm/dd/yyyy
   let formattedDate = formatDate(date.value);
 
   // Display on page based on whether its a future or past date
-  if (!isFutureDate){
+  if (!isFutureDate) {
     document.querySelector(
       "#total"
-    ).textContent = `Days since ${formattedDate}: ${days}`;
-  }else{
+    ).textContent = `It has been ${dates[0]} years, ${dates[1]} weeks, and ${dates[2]} days since ${formattedDate}`;
+  } else {
     document.querySelector(
       "#total"
-    ).textContent = `Days until ${formattedDate}: ${days*-1}`;
+    ).textContent = `It is ${dates[0]} years, ${dates[1]} weeks, and ${dates[2]} days until ${formattedDate}`;
   }
-  
 });
 
 // Calculate difference time difference between today and passed date
@@ -45,12 +47,22 @@ function formatDate(date) {
 
 // Convert milliseconds to specific format
 function convertMiliseconds(miliseconds, format) {
-  var days, hours, minutes, seconds, total_hours, total_minutes, total_seconds;
+  let days,
+    hours,
+    minutes,
+    seconds,
+    total_hours,
+    total_minutes,
+    total_seconds,
+    weeks,
+    years;
 
   total_seconds = parseInt(Math.floor(miliseconds / 1000));
   total_minutes = parseInt(Math.floor(total_seconds / 60));
   total_hours = parseInt(Math.floor(total_minutes / 60));
   days = parseInt(Math.floor(total_hours / 24));
+  weeks = parseInt(Math.floor(days / 7));
+  years = parseInt(Math.floor(days / 365));
 
   seconds = parseInt(total_seconds % 60);
   minutes = parseInt(total_minutes % 60);
@@ -66,6 +78,36 @@ function convertMiliseconds(miliseconds, format) {
     case "d":
       return days;
     default:
-      return { d: days, h: hours, m: minutes, s: seconds };
+      return { y: years, w: weeks, d: days, h: hours, m: minutes, s: seconds };
   }
+}
+
+// Convert milliseconds to specific format
+function altConvertMiliseconds(miliseconds) {
+  // Account for future dates
+  if (miliseconds < 1) {
+    miliseconds *= -1;
+  }
+  let days,
+    weeks,
+    total_days,
+    total_hours,
+    total_minutes,
+    total_seconds,
+    total_weeks,
+    years;
+
+  total_seconds = parseInt(Math.floor(miliseconds / 1000));
+  total_minutes = parseInt(Math.floor(total_seconds / 60));
+  total_hours = parseInt(Math.floor(total_minutes / 60));
+  total_days = parseInt(Math.floor(total_hours / 24));
+  total_weeks = parseInt(Math.floor(total_days / 7));
+  years = parseInt(Math.floor(total_days / 365));
+  console.log(miliseconds);
+  weeks = Math.floor((total_days - years * 365) / 7);
+  days = total_days - years * 365 - weeks * 7;
+  console.log(total_days);
+  console.log(`${years} ${weeks} ${days}`);
+
+  return [years, weeks, days];
 }
