a5f2060b0540567418ea437ce0cc3979f9bfb74c
Christopher-Adzima_lmig <Christopher.Adzima@LibertyMutual.com>
2023-04-21 17:52:44 -0400

    More accurate calculation of dates taking into account leap year and varying days per month

---
+++ assets/javascript/exactAge.js
@@ -0,0 +1,51 @@
+/**
+ * Exact Age
+ *
+ * Calculates the exact age for a given inputDate with JavaScript.
+ * This solution accounts for leap years and various days in each month.
+ * 
+ * Base code courtesy of Kevin Leary - https://www.kevinleary.net/blog/javascript-age-birthdate-mm-dd-yyyy/
+ */
+function exactAge(inputDate) {
+  let startDate = new Date(new Date(inputDate).toISOString().substr(0, 10));
+  const endingDate = new Date().toISOString().substr(0, 10); // YYYY-MM-DD
+  let endDate = new Date(endingDate);
+  if (startDate > endDate) {
+    const swap = startDate;
+    startDate = endDate;
+    endDate = swap;
+  }
+  const startYear = startDate.getFullYear();
+
+  // Leap years
+  const february =
+    (startYear % 4 === 0 && startYear % 100 !== 0) || startYear % 400 === 0
+      ? 29
+      : 28;
+  const daysInMonth = [31, february, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  let yearDiff = endDate.getFullYear() - startYear;
+  let monthDiff = endDate.getMonth() - startDate.getMonth();
+  let dayDiff = endDate.getDate() - startDate.getDate();
+
+  if (monthDiff < 0) {
+    yearDiff--;
+    monthDiff += 12;
+  }
+
+  if (dayDiff < 0) {
+    if (monthDiff > 0) {
+      monthDiff--;
+    } else {
+      yearDiff--;
+      monthDiff = 11;
+    }
+    dayDiff += daysInMonth[startDate.getMonth()];
+  }
+
+  return {
+    years: yearDiff,
+    months: monthDiff,
+    days: dayDiff,
+  };
+}
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -6,30 +6,27 @@ const date = document.querySelector("#date");
 
 // When the Calcuate button is clicked button is clicked
 calcDate.addEventListener("click", () => {
+
+  const exactTime = exactAge(date.value);
+
   // Get difference between entered dated and todays date in milliseconds
   let diff = calculateDays(date.value);
 
   // Figure out if its a future date or not
   let isFutureDate = diff < 0 ? true : false;
 
-  // Convert the milliseconds to days - use this if you want to just display days
-  let days = convertMiliseconds(diff, "d");
-
-  // this returns array with days, weeks, years - doesnt use months because of variability
-  let dates = altConvertMiliseconds(diff);
-
-  // Convert the input date to mm/dd/yyyy
+  // Convert the input string date to mm/dd/yyyy
   let formattedDate = formatDate(date.value);
 
   // Display on page based on whether its a future or past date
   if (!isFutureDate) {
     document.querySelector(
       "#total"
-    ).textContent = `It has been ${dates[0]} years, ${dates[1]} weeks, and ${dates[2]} days since ${formattedDate}`;
+    ).textContent = `It has been ${exactTime.years} years, ${exactTime.months} months, and ${exactTime.days} days since ${formattedDate}`;
   } else {
     document.querySelector(
       "#total"
-    ).textContent = `It is ${dates[0]} years, ${dates[1]} weeks, and ${dates[2]} days until ${formattedDate}`;
+    ).textContent = `It is ${exactTime.years} years, ${exactTime.months} months, and ${exactTime.days} days until ${formattedDate}`;
   }
 });
 
@@ -38,76 +35,9 @@ function calculateDays(date) {
   return new Date() - Date.parse(date);
 }
 
-// Format a date from yyyy-mm-dd to mm/dd/yyyy
+// Format a date string from yyyy-mm-dd to mm/dd/yyyy
 function formatDate(date) {
   let values = [];
   values = date.split("-");
   return `${values[1]}/${values[2]}/${values[0]}`;
-}
-
-// Convert milliseconds to specific format
-function convertMiliseconds(miliseconds, format) {
-  let days,
-    hours,
-    minutes,
-    seconds,
-    total_hours,
-    total_minutes,
-    total_seconds,
-    weeks,
-    years;
-
-  total_seconds = parseInt(Math.floor(miliseconds / 1000));
-  total_minutes = parseInt(Math.floor(total_seconds / 60));
-  total_hours = parseInt(Math.floor(total_minutes / 60));
-  days = parseInt(Math.floor(total_hours / 24));
-  weeks = parseInt(Math.floor(days / 7));
-  years = parseInt(Math.floor(days / 365));
-
-  seconds = parseInt(total_seconds % 60);
-  minutes = parseInt(total_minutes % 60);
-  hours = parseInt(total_hours % 24);
-
-  switch (format) {
-    case "s":
-      return total_seconds;
-    case "m":
-      return total_minutes;
-    case "h":
-      return total_hours;
-    case "d":
-      return days;
-    default:
-      return { y: years, w: weeks, d: days, h: hours, m: minutes, s: seconds };
-  }
-}
-
-// Convert milliseconds to specific format
-function altConvertMiliseconds(miliseconds) {
-  // Account for future dates
-  if (miliseconds < 1) {
-    miliseconds *= -1;
-  }
-  let days,
-    weeks,
-    total_days,
-    total_hours,
-    total_minutes,
-    total_seconds,
-    total_weeks,
-    years;
-
-  total_seconds = parseInt(Math.floor(miliseconds / 1000));
-  total_minutes = parseInt(Math.floor(total_seconds / 60));
-  total_hours = parseInt(Math.floor(total_minutes / 60));
-  total_days = parseInt(Math.floor(total_hours / 24));
-  total_weeks = parseInt(Math.floor(total_days / 7));
-  years = parseInt(Math.floor(total_days / 365));
-  console.log(miliseconds);
-  weeks = Math.floor((total_days - years * 365) / 7);
-  days = total_days - years * 365 - weeks * 7;
-  console.log(total_days);
-  console.log(`${years} ${weeks} ${days}`);
-
-  return [years, weeks, days];
 }
